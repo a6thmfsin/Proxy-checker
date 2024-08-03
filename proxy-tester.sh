8b6bec3f169030ba2670b9a819a61177f657fc25
greyhound <63813294+a6thmfsin@users.noreply.github.com>
2024-08-03 01:10:11 +0100

    Update proxy-tester.sh
    Changed the script so to be more accurate so 302 etc dont slipp by aswell added green colored text in stdout if proxy is working.

--- proxy-tester.sh
+++ proxy-tester.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+#Changed the script so to be more accurate so 302 etc dont slipp by aswell added green colored text in stdout if proxy is working.
 
-while IFS= read -r line; do
-    echo -n "$line: "
-    if curl -sS --connect-timeout 5 -x "$line:80" http://www.example.com -o /dev/null; then #can br any domain to test connection to
-        echo "Active"
+echo_colored() {
+    local color_code=$1
+    shift
+    local text=$@
+    echo -e "\e[${color_code}m${text}\e[0m"
+}
+
+GREEN="32"
+
+proxies=(
+    "http://154.12.242.82:8888"
+    "http://116.68.194.61:1080"
+    "http://62.109.31.192:20000"
+    "http://89.187.191.112:8888"
+    "http://13.208.56.180:80"
+    "http://152.26.231.22:9443"
+    "http://136.226.230.118:10230"
+    "http://136.226.233.31:10230"
+    "http://190.158.210.102:8080"
+        #proxies go here..
+)
+
+check_proxy() {
+    local proxy=$1
+    if curl -x "$proxy" -s --head http://www.google.com/ | grep "200 OK" > /dev/null; then
+        echo_colored $GREEN "Proxy $proxy is working"
     else
-        echo "Inactive"
+        echo "Proxy $proxy is not working"
     fi
-done < "proxies.txt"
+}
+
+for proxy in "${proxies[@]}"; do
+    check_proxy "$proxy"
+done
